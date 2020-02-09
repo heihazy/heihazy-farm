6c42ae89e26b6ce5d315a46fd2196e1ec9bd8fc2
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-02-09 22:03:20 +0200

    added loader
    some photos were hidden, need to figure out

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-
-
-
-function showPopup () {
-    var popup = document.getElementById('popup');
-    popup.classList.add('show')
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.classList.add("show");
 }
 
-function hidePopup () {
-    var popup = document.getElementById('popup');
-    popup.classList.remove('show')
+function hidePopup() {
+  var popup = document.getElementById("popup");
+  popup.classList.remove("show");
 }
+
+$("document").ready(function() {
+  $("body").prepend('<div class="spinner"></div>');
+  $(".site-wrap").addClass("hide");
+  $(".spinner")
+    .append('<div class="rect1"></div>')
+    .append('<div class="rect2"></div>')
+    .append('<div class="rect3"></div>')
+    .append('<div class="rect4"></div>')
+    .append('<div class="rect5"></div>');
+  setTimeout(function() {
+    $(".spinner").addClass("hide");
+    $(".site-wrap").removeClass("hide");
+  }, 2000);
+});
